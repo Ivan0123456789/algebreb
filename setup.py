c1548df99a7fc960b73239d296db4e2c914926cd
= <elite-a64@outlook.com>
2021-12-12 23:07:21 -0600

    Actualizando a version 4.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '3.0'
+VERSION = '4.0'
 DESCRIPTION = 'Biblioteca para generar expresiones y ejercicios de algebra'
 LONG_DESCRIPTION = 'Conjunto de funciones y clases para la creacion de ejercicios algebraicos'
 
